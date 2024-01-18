ee29d0828f15691d30da97857808db9bdaac8287
Anuj Panta <anuj1panta@gmail.com>
2024-01-17 19:42:09 -0800

    fixed the idea idea of resizing the height by the scroll view intead of the just the viewport, looks nice now, fixes bug jas found

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -10,11 +10,100 @@
             <Section title="Websites">
                 <div class="flex-col sm:flex sm:flex-row gap-2 my-2">
                     <div
-                        class="text-left py-3 px-5 rounded-lg hover:bg-stone-200/80 duration-300 text-sm"
+                        class="hover:bg-stone-200/80 text-left py-3 px-5 rounded-lg duration-300 text-sm"
                     >
-                        <p class="text-blue-600 font-semibold">
-                            Youtube Ripper
-                        </p>
+                        <div class="">
+                            <p class="text-blue-600 font-semibold sm:mx-auto">
+                                Youtube Ripper
+                            </p>
+                            <div
+                                class=" duration-100 my-0.5 px-3 py-1 rounded-lg flex gap-2 hover:bg-stone/80 mx-auto"
+                            >
+                                <a
+                                    href="/"
+                                    class=" duration-100 text-black hover:text-blue-600/80"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon icon-tabler icon-tabler-external-link"
+                                        width="16"
+                                        height="16"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        fill="none"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <path
+                                            stroke="none"
+                                            d="M0 0h24v24H0z"
+                                            fill="none"
+                                        />
+                                        <path
+                                            d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"
+                                        />
+                                        <path d="M11 13l9 -9" />
+                                        <path d="M15 4h5v5" />
+                                    </svg>
+                                </a>
+
+                                <a
+                                    href="/"
+                                    class=" duration-100 text-black hover:text-blue-600/80"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon icon-tabler icon-tabler-brand-github"
+                                        width="16"
+                                        height="16"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        fill="none"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <path
+                                            stroke="none"
+                                            d="M0 0h24v24H0z"
+                                            fill="none"
+                                        />
+                                        <path
+                                            d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+                                        />
+                                    </svg>
+                                </a>
+                                <a
+                                    href="/"
+                                    class=" duration-100 text-black hover:text-blue-600/80"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        class="icon icon-tabler icon-tabler-info-circle"
+                                        width="16"
+                                        height="16"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor"
+                                        fill="none"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <path
+                                            stroke="none"
+                                            d="M0 0h24v24H0z"
+                                            fill="none"
+                                        />
+                                        <path
+                                            d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"
+                                        />
+                                        <path d="M12 9h.01" />
+                                        <path d="M11 12h1v4h1" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
                         <p class="text-gray-600 my-2">
                             I believe media should be free, and mp3 are alwyas
                             fun to have ! You can search any youtube video and
